aa8dafc0a7e013c19a5b3f28059d277672d3b3ed
lisichkin.ivan2 <lisichkin.ivan2@rwb.ru>
2025-11-26 17:32:58 +0400

    prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,31 +2,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:prettier/recommended",
-    "plugin:eslint-comments/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "postcss.config.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'postcss.config.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
     ],
-    "react/react-in-jsx-scope": 0,
-    "no-unused-vars": 0,
-    "@typescript-eslint/no-unused-vars": 2,
-    "no-console": 0,
-    eqeqeq: 2,
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+    'no-console': 0,
+    eqeqeq: 2
   },
   settings: {
     react: {
-      version: "detect",
-    },
-  },
+      version: 'detect'
+    }
+  }
 };
