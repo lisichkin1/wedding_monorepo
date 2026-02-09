44e39b731ec8498ce57b3af7c6d7b2bc8ba68c5b
Ivan <ivanlisichkin@icloud.com>
2026-02-09 17:27:53 +0400

    add api get

--- services/backend/src/features/guests/rotes.ts
+++ services/backend/src/features/guests/rotes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 import { authenticateGuestRoutes } from '~/middleware/guest-auth';
 
-import { confirmGuest, createGuest } from './controller';
+import { confirmGuest, createGuest, deleteGuest } from './controller';
 
 const router = express.Router();
 // GET запросы - доступны фронтенду и боту
@@ -10,6 +10,7 @@ router.get('/guests', authenticateGuestRoutes);
 
 // POST /guests - только для бота
 router.post('/guests', authenticateGuestRoutes, createGuest);
+router.post('/guests', authenticateGuestRoutes, deleteGuest);
 
 // POST /guests/confirm/:token - доступна всем (через токен)
 router.post('/guests/confirm/:token', confirmGuest);
