eb983e9ca49be8982a154fd162f85e0c6fd08892
Ivan <ivanlisichkin@icloud.com>
2026-02-19 17:58:47 +0400

    fix link

--- services/frontend/src/shared/constants/vars.ts
+++ services/frontend/src/shared/constants/vars.ts
@@ -2,6 +2,6 @@ export const TIME = '2026-04-18 14:00';
 export const YANDEX_MAP_URL =
   'https://yandex.ru/maps/org/schastye/182761655792?si=nzpqe0gt8mx01twrd85750dhzg';
 export const YANDEX_MAP_APP =
-  'yandexmaps://maps.yandex.ru/maps/org/schastye/182761655792?si=nzpqe0gt8mx01twrd85750dhzg';
+  'yandexmaps://maps.yandex.com/?oid=182761655792&si=nzpqe0gt8mx01twrd85750dhzg';
 export const DGIS_URL = 'https://2gis.ru/saratov/geo/70000001023939834';
 export const DGIS_APP = 'dgis://2gis.ru/saratov/geo/70000001023939834';
